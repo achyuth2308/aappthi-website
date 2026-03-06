6f479bac6374fb6f3313c149c896d602d781ff8c
Shailaja Makkala <shailajap887@gmail.com>
2026-03-06 16:08:29 +0530

    changed sitemap

---
+++ public/sitemap.js
@@ -0,0 +1,25 @@
+export default function sitemap() {
+
+    const baseUrl =
+        (process.env.NEXT_PUBLIC_BASE_URL || "https://www.aapthisolutions.com").replace(/\/$/, "");
+
+    const routes = [
+        "",
+        "/about",
+        "/services",
+        "/projects",
+        "/global-exposure",
+        "/industry-engagements",
+        "/awards",
+        "/team",
+        "/careers",
+        "/contact",
+    ];
+
+    return routes.map((route) => ({
+        url: `${baseUrl}${route}`,
+        lastModified: new Date(),
+        changeFrequency: "monthly",
+        priority: route === "" ? 1 : 0.8,
+    }));
+}
